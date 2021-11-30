eedb0dfe4d937dee5eb8fae153e491187c5a6254
Guzik98 <guzkac2@gmail.com>
2021-11-30 19:29:44 +0100

    validate nested objects

--- src/offers/dto/create-offers.dto.ts
+++ src/offers/dto/create-offers.dto.ts
@@ -1,11 +1,12 @@
 import {
+  ArrayMaxSize,
   ArrayMinSize,
   IsArray,
   IsDefined,
-  IsEnum,
-  IsNotEmpty,
-  Max,
-  Min,
+  IsEnum, IsInt,
+  IsNotEmpty, IsNumber, IsOptional,
+  Max, MaxLength,
+  Min, minLength, MinLength,
   ValidateNested
 } from 'class-validator';
 import { MarkerIcon } from '../enums/marker_icon.enum';
@@ -30,48 +31,60 @@ export class CreateOffersDto {
   workplace_type: string;
   @IsNotEmpty({ message: 'Company name  is required' })
   company_name: string;
+  @IsOptional()
   company_url: string;
   @IsNotEmpty({ message: 'Company size is required' })
   company_size: string
   latitude: string;
   longitude: string;
   published_at: string;
+  @IsNotEmpty()
   remote_interview: boolean;
-  // @IsArray()
-  // @ArrayMinSize(1)
-  // @Type(() => SkillTypeDto)
-  // @ValidateNested({ each: true })
+  @IsArray()
+  @ArrayMinSize(1)
+  @ArrayMaxSize(3)
+  @IsDefined()
+  @ValidateNested({ each: true })
+  @Type(() => EmploymentTypeDto)
   employment_types : EmploymentTypeDto[]
   company_logo_url : string
   @IsEnum(ExperienceLevel)
   experience_level: string
   id:string
-  // @IsDefined()
-  // @IsArray()
-  // @ValidateNested()
-  // @ArrayMinSize(1)
-  // @Type(() => SkillTypeDto)
+  @IsDefined()
+  @ValidateNested({ each: true })
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => SkillTypeDto)
   skills: SkillTypeDto[]
+}
+
+export class SalaryTypeDto {
+  @IsOptional()
+  @Min(1000)
+  @Max(100000)
+  from: number
+  @IsOptional()
+  @Min(1000)
+  @Max(100000)
+  to: number
+  @IsOptional()
+  currency: string
 }
 
 export class EmploymentTypeDto {
   @IsEnum(EmploymentType)
   type: string
-  salary: SalaryTypeDto
-}
-
-export class SalaryTypeDto {
-  @Min(1000)
-  @Max(100000)
-  from: number
-  @Min(1000)
-  @Max(100000)
-  to: number
-  currency: string
+  @IsOptional()
+  @IsDefined()
+  @ValidateNested({ each: true })
+  salary: SalaryTypeDto | null
 }
 
 export class SkillTypeDto {
+  @IsNotEmpty()
   name: string;
+  @IsNumber()
   @Min(1)
   @Max(5)
   level: number;
